3c97ed1a0ef20dfc0b93c4d3470fb1f81019f111
idith almog <almog.idith@gmail.com>
2020-04-27 16:58:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  if array.length == 1 
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    array1 = array.pop
+    string1= array.join(",")
+    string=string1 +" and "+ array[-1]
+  end
 
 end
